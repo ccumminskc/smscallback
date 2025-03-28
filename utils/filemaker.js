6c3056826512b1c410100259371076acf473d8a7
ccumminskc <chris@portraitsbychristopher.com>
2025-03-27 22:17:45 -0500

    Create filemaker.js

---
+++ utils/filemaker.js
@@ -0,0 +1,25 @@
+const axios = require('axios');
+
+async function updateSmsStatusInFilemaker(data) {
+  const { messageSid, status, error, errorCode } = data;
+
+  const scriptPayload = {
+    script: 'Update SMS Status',
+    scriptParam: JSON.stringify({ messageSid, status, error, errorCode })
+  };
+
+  const response = await axios.post(
+    process.env.FILEMAKER_SCRIPT_URL,
+    scriptPayload,
+    {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${process.env.FILEMAKER_API_TOKEN}`
+      }
+    }
+  );
+
+  return response.data;
+}
+
+module.exports = { updateSmsStatusInFilemaker };
