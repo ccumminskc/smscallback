4b5af125d80105df50ccdf8c64385dd4d3d6d1c5
ccumminskc <chris@portraitsbychristopher.com>
2025-03-27 22:20:27 -0500

    Update index.js
    Added basic starter code fro chatgpt

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const twilioStatus = require('./routes/twilioStatus');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use('/', twilioStatus);
+
+app.listen(port, () => {
+  console.log(`Webhook server listening on port ${port}`);
+});
 
