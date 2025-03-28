7d538abcdb725369ad8c843a6187ad32444b5ecd
ccumminskc <chris@portraitsbychristopher.com>
2025-03-28 00:10:20 -0500

    Update filemaker.js
    convert to GET request.

--- utils/filemaker.js
+++ utils/filemaker.js
@@ -4,22 +4,23 @@ const { getFilemakerToken } = require('./auth');
 async function updateSmsStatusInFilemaker({ messageSid, status, error, errorCode }) {
   const token = await getFilemakerToken();
 
-  const scriptParam = JSON.stringify({
+  const scriptName = encodeURIComponent('Update SMS Status');
+  const layoutName = encodeURIComponent('Text Messages');
+  const database = encodeURIComponent(process.env.FILEMAKER_DATABASE);
+
+  const scriptParam = encodeURIComponent(JSON.stringify({
     messageSid,
     status,
     error,
     errorCode
-  });
+  }));
 
-  const url = `${process.env.FILEMAKER_SERVER_URL}/fmi/data/v1/databases/${process.env.FILEMAKER_DATABASE}/layouts/Text%20Messages/script`;
+  const url = `${process.env.FILEMAKER_SERVER_URL}/fmi/data/v1/databases/${database}/layouts/${layoutName}/script/${scriptName}?script.param=${scriptParam}`;
 
-  const response = await axios.post(url, {}, {
+  const response = await axios.get(url, {
     headers: {
-      'Content-Type': 'application/json',
       'Authorization': `Bearer ${token}`,
-      'FM-Data-API-Version': 'v1',
-      'script': 'Update SMS Status',
-      'script.param': scriptParam
+      'Content-Type': 'application/json'
     }
   });
 
