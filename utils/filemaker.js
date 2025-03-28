bde4b616a619d4170f5bea07dcad9dad7a10cfd0
ccumminskc <chris@portraitsbychristopher.com>
2025-03-28 00:02:52 -0500

    Update filemaker.js

--- utils/filemaker.js
+++ utils/filemaker.js
@@ -4,22 +4,22 @@ const { getFilemakerToken } = require('./auth');
 async function updateSmsStatusInFilemaker({ messageSid, status, error, errorCode }) {
   const token = await getFilemakerToken();
 
-  const scriptPayload = {
-    script: 'Update SMS Status',
-    scriptParam: JSON.stringify({
-      messageSid,
-      status,
-      error,
-      errorCode
-    })
-  };
+  const scriptParam = JSON.stringify({
+    messageSid,
+    status,
+    error,
+    errorCode
+  });
 
-  const url = `${process.env.FILEMAKER_SERVER_URL}/fmi/data/v1/databases/${process.env.FILEMAKER_DATABASE}/scripts`;
+  const url = `${process.env.FILEMAKER_SERVER_URL}/fmi/data/v1/databases/${process.env.FILEMAKER_DATABASE}/layouts/Text%20Messages/script`;
 
-  const response = await axios.post(url, scriptPayload, {
+  const response = await axios.post(url, {}, {
     headers: {
       'Content-Type': 'application/json',
-      'Authorization': `Bearer ${token}`
+      'Authorization': `Bearer ${token}`,
+      'FM-Data-API-Version': 'v1',
+      'script': 'Update SMS Status',
+      'script.param': scriptParam
     }
   });
 
